453743c4b0b468f84ecea56b42ac9fc700dedab0
bizy-coder <zydney.biz11@gmail.com>
2024-06-05 13:56:49 -0400

    Project Setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Scrollbar from "./Scrollbar.svelte";
+</script>
+
+
+<Scrollbar/>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
